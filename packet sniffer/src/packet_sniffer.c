00aae3ea41539aad4ae06755c82a58756d528021
Nave Adi <61701327+Terrfex@users.noreply.github.com>
2021-10-30 04:35:06 +0300

    added ip&MAC display

--- packet sniffer/src/packet_sniffer.c
+++ packet sniffer/src/packet_sniffer.c
@@ -18,7 +18,30 @@ ErrorCode handle_packet(const uint8_t* pkt_buffer, uint16_t pkt_length)
 		goto cleanup;
 	}
 
-	printf("first byte : %x, length: %u\n", pkt_buffer[0] , pkt_length);//prints the size of the packet
+	struct ethhdr* eth = (struct ethhdr*)pkt_buffer;
+
+	if(ntohs(eth->h_proto) == ETH_P_IP)
+	{
+		struct iphdr* iph = (struct iphdr*)(pkt_buffer + sizeof(struct ethhdr));
+
+		struct sockaddr_in src_ip = { 0 };
+		src_ip.sin_addr.s_addr = iph -> saddr;
+		char src_ip_str[MAX_IP_LEN] = { 0 };
+		strcpy(src_ip_str, inet_ntoa(src_ip.sin_addr));
+
+		struct sockaddr_in dst_ip = { 0 };
+		dst_ip.sin_addr.s_addr = iph -> daddr;
+		char dst_ip_str[MAX_IP_LEN] = { 0 };
+		strcpy(dst_ip_str, inet_ntoa(dst_ip.sin_addr));
+
+		printf("%s (%.2x:%.2x:%.2x:%.2x:%.2x:%.2x) ----> %s (%.2x:%.2x:%.2x:%.2x:%.2x:%.2x) ", src_ip_str, eth -> h_source[0], eth -> h_source[1], eth -> h_source[2], eth -> h_dest[3], eth -> h_source[4], eth -> h_source[5], 
+		dst_ip_str, eth -> h_dest[0], eth -> h_dest[1], eth -> h_dest[2], eth -> h_dest[3], eth -> h_dest[4], eth -> h_dest[5]);
+	}else
+	{
+		printf("%.2x:%.2x:%.2x:%.2x:%.2x:%.2x ----> %.2x:%.2x:%.2x:%.2x:%.2x:%.2x ", eth -> h_source[0], eth -> h_source[1], eth -> h_source[2], eth -> h_dest[3], eth -> h_source[4], eth -> h_source[5], 
+		eth -> h_dest[0], eth -> h_dest[1], eth -> h_dest[2], eth -> h_dest[3], eth -> h_dest[4], eth -> h_dest[5]);
+	}
+	printf("[%u]\n", pkt_length);
 
  cleanup:
 	return error_code;
@@ -41,31 +64,3 @@ ErrorCode sniff_packets(const char* interface_name)
 	if(setsockopt(raw_socket, SOL_SOCKET, SO_BINDTODEVICE, interface_name, strlen(interface_name)) == -1)//setting the sock options and interface
 	{
 		perror("setsockopt");
-		error_code = ERROR_SOCKET;
-		goto cleanup;
-	}
-
-	while(1)//sniffing the packets
-	{
-		data_size = recvfrom(raw_socket, packet_buffer, PACKET_BUFFER_SIZE, 0, NULL, NULL);
-		if (data_size == -1)//make sure we dint have an error
-		{
-			error_code = ERROR_RECV_FROM;
-			perror("recvfrom");
-			goto cleanup;
-		}
-		error_code = handle_packet(packet_buffer, data_size);
-		if (error_code != ERROR_SUCCESS)
-		{
-			printf("Handle packt failed with: %d\n", error_code);
-			goto cleanup;
-		}
-	}
-
-cleanup://make sure we wont leak any resources
-	if (raw_socket != -1 && close(raw_socket) == -1)
-	{
-		perror("close");
-	}
-	return error_code;
-}
